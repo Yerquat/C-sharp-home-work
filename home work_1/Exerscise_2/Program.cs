826c1cd87e2c0aa5972b83ca8041428307b6c12e
Yerquat <yernur75@gmail.com>
2022-12-02 03:31:56 +0600

    наибольшее из 3 чисел

---
+++ home work_1/Exerscise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int numberA = new Random().Next(0, 100);
+Console.WriteLine(numberA);
+
+int numberB = new Random().Next(0, 100);
+Console.WriteLine(numberB);
+
+int numberC = new Random().Next(0, 100);
+Console.WriteLine(numberC);
+
+ // numberA != numberB; как поставить это условие в код?
+int max = numberA;
+
+
+if (numberB > max) max = numberB;
+
+if (numberC > max) max = numberC;
+
+//if (numberС > max) max = numberС;
+
+Console.WriteLine($"MAX = {max}");
+
