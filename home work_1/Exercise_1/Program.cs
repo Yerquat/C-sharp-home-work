33fe8f6f9a989f781677b5a4301076b2f1cad0a2
Yerquat <yernur75@gmail.com>
2022-12-02 01:08:03 +0600

    Max

---
+++ home work_1/Exercise_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int numberA = new Random().Next(0, 100);
+int numberB = new Random().Next(0, 100);
+
+int max = numberA;
+if (numberA > numberB) 
+    numberA = max;
+else
+    numberB = max;
+    Console.WriteLine(numberA);
+    Console.WriteLine(numberB);
+    Console.WriteLine(max);
