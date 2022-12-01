79c1febcee8e9191413f8f6fa640ebc2d4879fdb
Yerquat <yernur75@gmail.com>
2022-12-02 04:00:10 +0600

    четное и нечетное

---
+++ home work_1/Exercise_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+Console.WriteLine("Число a - четное");
+if (b != 0)
+Console.WriteLine("Число b - нечетное");
+Console.WriteLine (b);
