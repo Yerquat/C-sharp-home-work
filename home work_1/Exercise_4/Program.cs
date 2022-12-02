b7a27f3f55b5c0d0570d23756da3a4896a713acd
Yerquat <yernur75@gmail.com>
2022-12-02 22:29:32 +0600

    Exercise_4

---
+++ home work_1/Exercise_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число >= 2: ");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+
+while (i <= N)
+{    
+//int b = i;
+Console.WriteLine($"{i}");
+i = i + 2;
+}
